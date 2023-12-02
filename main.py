e86e71094e25f2ec70cf0525498216045740b706
Gabriel Teuchert <gabriel.teuchert@gmail.com>
2023-12-02 23:53:56 +0100

    dayOne done

--- main.py
+++ main.py
@@ -5,12 +5,55 @@ import numpy as np
 
 
 class mapReader:
-    def __init__(self):
-        self.calibrationDataPath = "src/calibrationData.txt"
+
+    calibrationData = []
+    digimons = []
+
+    def __init__(self, path=None):
+        self.dataPath = path
+        self.readCalibrationData()
+        self.calibration = int(sum(self.digimonCatcher()))
 
     def readCalibrationData(self):
-        path = self.calibrationDataPath
+        data = []
+        with open(self.dataPath) as textData:
+            for line in textData:
+                data.append(line.strip())
+        self.calibrationData = data
+        return self.calibrationData
+
+    def digimonCatcher(self):
+        digits = np.zeros(len(self.calibrationData))
+        firstDigit = []
+        secondDigit = []
+        digitBuffer = []
+        for index, line in enumerate(self.calibrationData):
+            digimons = ''.join((digimon if digimon in '0123456789' else ' ') for digimon in line)
+            listOfNumbers = digimons.split()
+            for element in listOfNumbers:
+                if len(firstDigit) == 0:
+                    firstDigit.append(element)
+                else:
+                    digitBuffer.append(element)
+            if len(digitBuffer) == 0:
+                digit = firstDigit + firstDigit
+                digits[index] = float(''.join(digit))
+                firstDigit = []
+                secondDigit = []
+                digitBuffer = []
+            elif len(digitBuffer) != 0:
+                secondDigit.append(digitBuffer.pop(-1))
+                digit = firstDigit + secondDigit
+                digits[index] = float(''.join(digit))
+                firstDigit = []
+                secondDigit = []
+                digitBuffer = []
+            else:
+                digits[index] = float(0)
+
+        return digits
 
 
 if __name__ == '__main__':
-    pass
+    mR = mapReader(path='src/calibrationData.txt')
+    print('Calibration Data = {0}'.format(mR.calibration))
---
+++ tests/dayOne/test_dayOne.py
@@ -0,0 +1,25 @@
+# Gabriel Teuchert
+
+import unittest
+from main import mapReader as mR
+
+
+test_calibrationData = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+test_digimon = [12, 38, 12345, 7]
+test_digits = [12, 38, 15, 77]
+test_result = 142
+
+
+class test_classDayOne(unittest.TestCase):
+
+    def testReader(self):
+        calibrationData = mR(path="exampleInput.txt")
+        self.assertEquals(calibrationData.readCalibrationData(), test_calibrationData, 'The reading went wrong')
+
+    def test_DigimonCatcher(self):
+        digimons = mR(path="exampleInput.txt")
+        self.assertEquals(digimons.digimonCatcher().tolist(), test_digits, 'The Digimons are escalating')
+
+
+if __name__ == "__main__":
+    unittest.main()
