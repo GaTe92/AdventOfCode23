221e0e4de6fe1a3a6c113119e56e4c0165041806
Gabriel Teuchert <gabriel.teuchert@gmail.com>
2023-12-02 14:03:27 +0100

    set up project structure

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# Advent of Code 2023
+# Gabriel Teuchert
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
+class mapReader:
+    def __init__(self):
+        self.calibrationDataPath = "src/calibrationData.txt"
+
+    def readCalibrationData(self):
+        path = self.calibrationDataPath
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
